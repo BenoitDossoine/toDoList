f222518113fbc494307a979f744403919e99e150
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-04-08 21:44:04 +0200

    Added a init function to initialize all buttons. Added a submitHandler function to handle the submits from the form. Added a createItem function.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import Item from "./Item.js";
+
+const app = {
+    init() {
+        const toDoForm = document.getElementById("inputForm");
+        toDoForm.addEventListener("submit", this.submitHandler);
+    },
+
+    submitHandler(e) {
+        e.preventDefault();
+
+        const toDoField = document.getElementById("toDoField");
+        const toDoItem = toDoField.value;
+        app.createItem(toDoItem);
+        toDoField.value = "";
+    },
+
+    createItem(item){
+        const newItem = new Item(item);
+        console.log(newItem.htmlString);
+    }
+};
+
+app.init();
