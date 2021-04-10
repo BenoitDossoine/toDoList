e8c304786c8b156a0434be5efbd365c9a7860d93
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-04-10 19:02:39 +0200

    To do items are now stored into the local storage and are displayed correctly

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const app = {
     init() {
         const toDoForm = document.getElementById("inputForm");
         toDoForm.addEventListener("submit", this.submitHandler);
+
+        const clearButton = document.getElementById("clearAll");
+        this.fetchList();
     },
 
     submitHandler(e) {
@@ -17,10 +20,41 @@ const app = {
         toDoField.value = "";
     },
 
-    createItem(item){
+    fetchList() {
+        const list = JSON.parse(localStorage.getItem("toDoList"));
+        const toDoContainer = document.getElementById("toDoList");
+        toDoContainer.innerHTML = "";
+        if (list === null) {
+            const emptyArr = JSON.stringify([]);
+            localStorage.setItem("toDoList", emptyArr);
+        } else {
+            
+            list.forEach(function (value) {
+                const item = value;
+                const html = document.createElement("div.toDoItem");
+                html.innerHTML = `
+               <p>${item._item}</p>
+               <div class="item-icons">
+                   <a href="#"><i class="far fa-check-circle"></i></a>
+                   <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
+                   <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
+               </div>`;
+                toDoContainer.appendChild(html);
+            });
+        }
+
+
+
+
+    },
+
+    createItem(item) {
         const newItem = new Item(item);
-        const itemList = document.getElementById("toDoList");
-        itemList.innerHTML += newItem.htmlString;
+        console.log(newItem.htmlString);
+        const listStored = JSON.parse(localStorage.getItem("toDoList"));
+        listStored.push(newItem);
+        localStorage.setItem("toDoList", JSON.stringify(listStored));
+        this.fetchList();
     }
 };
 
