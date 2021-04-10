9cb65feea22c499be9b3cab7cb1b04be9332d9ab
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-04-10 19:21:04 +0200

    Added data-item attributes to buttons

--- js/Item.js
+++ js/Item.js
@@ -1,22 +1,21 @@
 "use strict";
 
 class Item {
-    constructor(item){
+    constructor(item) {
         this._item = item;
         this._dueDate = new Date();
         this._status = false;
-    }
-
-    get htmlString() {
-        return `<div class="toDoItem">
+        this._htmlString = `<div class="toDoItem">
         <p>${this._item}</p>
         <div class="item-icons">
-            <a href="#"><i class="far fa-check-circle"></i></a>
-            <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
-            <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
+            <a href="#" data-item="${this._item}"><i class="far fa-check-circle"></i></a>
+            <a href="#" data-item="${this._item}" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
+            <a href="#" data-item="${this._item}" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
         </div>
     </div>`;
     }
+
+    
 }
 
 export default Item;
