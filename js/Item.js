998fee04107ad8e122ceb1ec0323bcb7d48a1877
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-04-11 13:23:56 +0200

    Added classname "check-item" to the check button for each item

--- js/Item.js
+++ js/Item.js
@@ -8,7 +8,7 @@ class Item {
         this._htmlString = `<div class="toDoItem">
         <p>${this._item}</p>
         <div class="item-icons">
-            <a href="#" data-item="${this._item}"><i class="far fa-check-circle"></i></a>
+            <a href="#" data-item="${this._item}"><i class="check-item far fa-check-circle"></i></a>
             <a href="#" data-item="${this._item}" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
             <a href="#" data-item="${this._item}" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
         </div>
